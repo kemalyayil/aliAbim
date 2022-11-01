cb79b36d7f6fe7618bf8fc5465188a58ee37866d
kemalyayil <82785245+kemalyayil@users.noreply.github.com>
2022-11-01 12:44:30 +0100

    yeni kod

--- src/calisma.java
+++ src/calisma.java
@@ -1,5 +1,6 @@
 public class calisma {
     public static void main(String[] args) {
         System.out.println("aliAbimin Bir Ciftligi var");
+        System.out.println("bombili bili bili bom");
     }
 }
